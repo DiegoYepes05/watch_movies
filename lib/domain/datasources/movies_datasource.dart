e1983f2d0364647ad5c6792c1e9763783a0e4e44
Diego Yepes <diego.yepes.dev.05@gmail.com>
2024-02-16 18:06:36 -0500

    Environment variables configure

---
+++ lib/domain/datasources/movies_datasource.dart
@@ -0,0 +1,5 @@
+import 'package:watchmovies/domain/entities/movie.dart';
+
+abstract class MoviesDatasource {
+  Future<List<Movie>> getNowPlaying({int page = 1});
+}
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'package:watchmovies/constants/environment.dart';
+
 class HomeScreen extends StatelessWidget {
   static const name = 'home-movies';
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Placeholder(),
-    );
+    return Scaffold(
+        body: Center(
+      child: Text(Environment.theMovieDbKey),
+    ));
   }
 }
